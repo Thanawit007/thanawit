826bef950b56ef83a9c66f0e851661b73326b2d3
10305-3 <a10305-3@iMac-khxng-10305-3.local>
2018-07-17 14:13:28 +0700

    Start App

---
+++ app/src/main/java/com/example/a10305_3/myfristapplab1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.a10305_3.myfristapplab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+//ประกาศตัวแปร
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //เขียนโค้ช
+
+    }
+}
